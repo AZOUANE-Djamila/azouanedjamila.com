5f2ed20cf2f5be7846065984282df6c22ab7e296
djamila <djamila@bnmconsultant.com>
2024-11-22 15:47:23 +0100

    Added elements to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Hero from '../components/Hero';
+import ExperienceSection from './Experience';
+import Contact from './Contact';
 
 
 const App: React.FunctionComponent = () => {
   return (
       <Layout>
           <Hero />
+          <ExperienceSection />
+          <Contact />
       </Layout>
   );
 };
